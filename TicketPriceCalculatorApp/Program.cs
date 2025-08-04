eddb4968bbac7a7c3984675ee9ad91cf91d1ecc6
godfred michael ohene <gmohene@st.ug.edu.gh>
2025-08-04 20:38:46 +0000

    Added all three apps: Grade Calculator, Ticket Price Calculator, Triangle Type Identifier

---
+++ TicketPriceCalculatorApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicketPriceCalculatorApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 12 || age >= 65)
+                Console.WriteLine("Ticket Price: GHC7");
+            else
+                Console.WriteLine("Ticket Price: GHC10");
+
+            Console.ReadLine(); // Keeps the window open
+        }
+    }
+}
